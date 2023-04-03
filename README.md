4c4bd00c7b8dba0e8a50d6941a722da0453b8245
Rockoban420 <lautaro.avellaneda@gmail.com>
2023-04-02 18:06:37 -0700

    creadte readme license missing

---
+++ README.md
@@ -0,0 +1,32 @@
+# asd
+
+## Description
+    asd
+
+## Installation
+asd
+
+## Usage
+asd
+
+## Contributions
+asd
+
+## Tests
+asd
+
+## License
+Squirtle
+
+## Questions
+If you would like to contact me please email me at asd
+And my github profile is ![here](https://github.com/asd)
+
+## Table of Contents (Optional)
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contributions](#Contributions)
+- [License](#License)
+- [Questions](#Questions)
--- index.js
+++ index.js
@@ -1,12 +1,110 @@
 // TODO: Include packages needed for this application
 
+// process.argv
 
+const inquirer = require ('inquirer');
+const fs = require ('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+
+inquirer.prompt ([
+    {
+        name: 'projectTitle',
+        message: 'Enter your project title',
+        type: 'input'
+    }, 
+    {
+        name: 'description',
+        message: 'Enter the description of your project',
+        type: 'input'
+    }, 
+    {
+        name: 'installation',
+        message: 'Enter installation instructions',
+        type: 'input'
+    },
+    {
+        name: 'usage',
+        message: 'Enter the usage information',
+        type: 'input'
+    },
+    {
+        name: 'contribution',
+        message: 'Enter the contribution guidelines',
+        type: 'input'
+    },
+    {
+        name: 'test',
+        message: 'Enter test instructions',
+        type: 'input'
+    },
+    {
+        name: 'license',
+        message: 'Which license would you like to use?',
+        type: 'list',
+        choices: ['Bulbasaour', 'Squirtle', 'Charmander']
+    },
+    {
+        name: 'username',
+        message: 'Enter your github username',
+        type: 'input'
+    },
+    {
+        name: 'email',
+        message: 'Enter your email address',
+        type: 'input'
+    },
+   
+   
+
+]).then (answers => {
+    console.log(answers);
+
+
+    fs.writeFile ('README.md', fileGenerator(answers), (err)=>{
+        if (err){
+            return console.log(err);
+        }
+        console.log ('Success!');
+    })
+});
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const fileGenerator = ({projectTitle, description, installation, usage, contribution, test, license, username, email, ...retstOfCrap}) => {
+    console.log (retstOfCrap);
+    return `# ${projectTitle}
+
+## Description
+${description}
+
+## Installation
+${installation}
+
+## Usage
+${usage}
+
+## Contributions
+${contribution}
+
+## Tests
+${test}
+
+## License
+${license}
+
+## Questions
+If you would like to contact me please email me at ${email}
+And my github profile is ![here](https://github.com/${username})
+
+## Table of Contents (Optional)
+
+- [Description](#Description)
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contributions](#Contributions)
+- [License](#License)
+- [Questions](#Questions)`;
+}
 
 // TODO: Create a function to initialize app
 function init() {}
